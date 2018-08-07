6c1d8b77dd03c867ba35751f5966e2fb08a4cee5
Jerry <random4423.student@gmail.com>
2018-08-07 12:24:09 -0500

    Worked at home
    pulled from git

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    
     //MARK: Initialization
     
     override init(frame: CGRect) {
@@ -13,9 +14,16 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    func ratingButtonTapped(button: UIButton)
+    {
+        print("Button pressed 👍")
+    }
+    
     //MARK: Private Methods
     
     private func setupButtons() {
+        // Create the button
         let button = UIButton()
         button.backgroundColor = UIColor.red
         
@@ -23,6 +31,10 @@ class RatingControl: UIStackView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action:
+            #selector(RatingControl.ratingButtonTapped(button:)),for: .touchUpInside)
         
         //Add the button to the stack
         addArrangedSubview(button)
